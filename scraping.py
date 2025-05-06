ba0d2c001a0857ddf1b5c592f188ebc5f6cb985a
Michael-Botelho-Dev <mbotelho2303@gmail.com>
2025-05-06 11:57:05 -0300

    feat: get categories

--- scraping.py
+++ scraping.py
@@ -11,12 +11,22 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 final_response = {}
 
+# title
 final_response["title"] = soup.select_one('h2#product_title').get_text()
 
+# brand
 final_response["brand"] = soup.select_one('.brand').get_text()
+
+# categories
+categories = soup.select("nav.current-category a")
+final_response["categories"] = [cat.get_text() for cat in categories]
+
 
 # export
 with open ('produto.json', 'w',encoding='utf-8') as f:
     json.dump(final_response, f, indent=2)
 
 print("✅ Sucesso.")
+
+# criar um for para soup.select("nav.current-category a") SALVAR dentro de uma lista e "categories" vai receber esta variavel como lista 
+# tenho que criar uma list categories 
