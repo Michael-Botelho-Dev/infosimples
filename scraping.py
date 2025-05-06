180b83808fbf6183063dcbe476471c7597534bd0
Michael-Botelho-Dev <mbotelho2303@gmail.com>
2025-05-06 00:21:26 -0300

    feat: get title and get brand

--- scraping.py
+++ scraping.py
@@ -1,21 +1,22 @@
 import requests
 import json
+from bs4 import BeautifulSoup
 
+# request
 url = 'https://infosimples.com/vagas/desafio/commercia/product.html'
 response = requests.get(url)
 
-final_response = {
-    "title": "",
-    "brand": "",
-    "categorie": [],
-    "description": "",
-    "skus": [],
-    "properties": [],
-    "reviews": [],
-    "reviews_average_score": 0.0,
-    "url":  "https://infosimples.com/vagas/desafio/commercia/product.html"
+# parsing with BeautfulSoup
+soup = BeautifulSoup(response.text, 'html.parser')
 
-}
+final_response = {}
 
+final_response["title"] = soup.select_one('h2#product_title').get_text()
+
+final_response["brand"] = soup.select_one('.brand').get_text()
+
+# export
 with open ('produto.json', 'w',encoding='utf-8') as f:
     json.dump(final_response, f, indent=2)
+
+print("✅ Sucesso.")
