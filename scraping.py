ef41e3add94fc9c0a0b948dbd9c0f779adb7dd55
Michael-Botelho-Dev <mbotelho2303@gmail.com>
2025-05-06 15:02:06 -0300

    feat: get description and url

--- scraping.py
+++ scraping.py
@@ -21,6 +21,12 @@ final_response["brand"] = soup.select_one('.brand').get_text()
 categories = soup.select("nav.current-category a")
 final_response["categories"] = [cat.get_text() for cat in categories]
 
+# description 
+final_response["description"] = soup.select_one('.proddet p').get_text()
+
+
+# url
+final_response["url"] = url 
 
 # export
 with open ('produto.json', 'w',encoding='utf-8') as f:
@@ -28,5 +34,4 @@ with open ('produto.json', 'w',encoding='utf-8') as f:
 
 print("✅ Sucesso.")
 
-# criar um for para soup.select("nav.current-category a") SALVAR dentro de uma lista e "categories" vai receber esta variavel como lista 
-# tenho que criar uma list categories 
+
