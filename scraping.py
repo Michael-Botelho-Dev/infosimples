dc658eb206c0f11c1a8ca216c9cf1c81e9443165
Michael-Botelho-Dev <mbotelho2303@gmail.com>
2025-05-07 00:29:02 -0300

    feat: get reviws average score and fix description

--- scraping.py
+++ scraping.py
@@ -22,16 +22,21 @@ categories = soup.select("nav.current-category a")
 final_response["categories"] = [cat.get_text() for cat in categories]
 
 # description 
-final_response["description"] = soup.select_one('.proddet p').get_text()
+final_response["description"] = soup.select_one('.proddet p').get_text(strip=True)
 
+# skus
+final_response["skus"] = []
+
+
+# reviews_average_score
+score = soup.select_one('#comments h4')
+final_response["reviews_average_score"] = float(score.get_text().split(":")[1].split("/")[0].strip())
 
 # url
 final_response["url"] = url 
 
 # export
 with open ('produto.json', 'w',encoding='utf-8') as f:
-    json.dump(final_response, f, indent=2)
+    json.dump(final_response, f, indent=2, ensure_ascii=False)
 
 print("✅ Sucesso.")
-
-
